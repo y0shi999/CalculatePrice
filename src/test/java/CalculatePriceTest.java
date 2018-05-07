af4b20044ac2265713284c4f4366f400e2a75ec6
no user <no email>
2018-05-07 11:18:29 +0200

    feat(calculatePrice) shouldReturnFullPriceInNominalCaseTwin - Introducing Formula enum

--- src/test/java/CalculatePriceTest.java
+++ src/test/java/CalculatePriceTest.java
@@ -1,11 +1,19 @@
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatePriceTest {
 
+
     @Test
     public void shouldReturnFullPriceInNominalCaseSingle() {
-        Assertions.assertThat(PriceCalculator.calculatePrice("Single"))
+        assertThat(PriceCalculator.calculatePrice(Formula.SINGLE))
             .isEqualTo(PriceCalculator.SINGLE_FULL_PRICE);
+    }
+
+    @Test
+    public void shouldReturnFullPriceInNominalCaseTwin() {
+        assertThat(PriceCalculator.calculatePrice(Formula.TWIN))
+            .isEqualTo(PriceCalculator.TWIN_FULL_PRICE);
     }
 }
